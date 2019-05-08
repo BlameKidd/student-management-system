5f2c08c514fea22642595c7dfb9515f151f07080
Conan <lipeiguan@gmail.com>
2019-05-08 09:53:52 +0800

    update

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from app.models import Classes
 
 
 def classes_list(request):
-
-    return
+    classes_obj = Classes.objects.all()
+    return render(request, 'classes/classes_list.html', {'classes_obj': classes_obj})
